45e2a329e2b501472f468866df064e06a7e087ba
USER <pnina6160@gmail.com>
2024-01-04 17:09:57 +0200

    hw redux lesson 14

--- src/library/reducers/book.js
+++ src/library/reducers/book.js
@@ -1,6 +1,6 @@
 const initialState = {
     arrBook: [{ code: 1, name: "Shikopizki", price: 42, aouther: "Shifra Glik", imgUrl: "../../images/2080602-2.jpg" },
-    { code: 2, name: "ShiriMiri", price: 20, aouther: "Menoch Fuchs", imgUrl:"../../images/e9f25ec88c7d02b6c92b661030facecc_300x300.webp" },
+    { code: 2, name: "ShiriMiri", price: 20, aouther: "Menoch Fuchs", imgUrl: "../../images/e9f25ec88c7d02b6c92b661030facecc_300x300.webp" },
     { code: 3, name: "Ptzatza Metakteket", price: 33, aouther: "Peher", imgUrl: "../../images/e46bebac211d0bd188ec42fc88492196.jpg" },
     { code: 4, name: "Dudi Veudi", price: 20, aouther: "Sara Leon", imgUrl: "../../images/66ae3b99e854cb3b8eee496a4899e8e9.jpg" },
     { code: 5, name: "Meolamam Shel Yeladim", price: 38, aouther: "Dan melech", imgUrl: "../../images/351044.jpeg" },
@@ -42,12 +42,17 @@ export const bookReducer = (state = initialState, action) => {
                     return item
                 return action.payload
             })
+            let newSelectedBook
+            if (state.selectedBook.code === action.payload.code)
+                newSelectedBook = action.payload
+            else
+                newSelectedBook = state.selectedBook
             return {
-                selectedBook: state.selectedBook,
+                selectedBook: newSelectedBook,
                 selectedBookToEdit: null,
                 arrBook: copy
             }
-            default:
-                return state;
+        default:
+            return state;
     }
 }
